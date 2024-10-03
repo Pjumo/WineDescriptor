52869ac9f36c5b825d96be2e366878e2b966c17c
Pjumo <parkjunmoa@naver.com>
2024-10-03 10:38:44 +0900

    requirements

--- llm.py
+++ llm.py
@@ -35,7 +35,7 @@ def get_retriever():
 
 
 def get_llm(model_name):
-    keras_backend: str = "tensorflow"
+    keras_backend: str = "jax"
     llm = GemmaLocalKaggle(
         model_name=model_name,
         keras_backend=keras_backend,
